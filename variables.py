81960c4b3738c458a255e3cc64124a92a0659fe4
Learning-stud <jayahalpara123456@gmail.com>
2023-11-24 11:06:14 +0530

    .......

---
+++ variables.py
@@ -0,0 +1,13 @@
+# strings
+name = "jay"
+print(type(name))
+# int
+roll_no= 5
+print(type(roll_no))
+# float
+percentage= 95.8
+print(type(percentage))
+# boolean
+is_student= True
+print(type(is_student))
+print(name,roll_no,percentage,is_student)
