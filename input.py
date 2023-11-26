d692ee23cbab2fa7dc5be1d16264b7cf92c3ff46
Learning-stud <jayahalpara123456@gmail.com>
2023-11-26 19:40:48 +0530

    ..

--- input.py
+++ input.py
@@ -1,2 +1,21 @@
-name = input("Please enter your name :")
-print(name)
+# Input in python
+# name = input("Please enter your name :")
+# print(name)
+
+
+# first type of input.
+
+# age = input("Enter your age")
+# print( type(age))
+
+# second type of input by usinng type casting.
+# age = int(input("Enter your age"))
+# print(type(age))
+
+
+#  sum of two given number using input.
+
+sum1= int (input( "\n Enter your value 1 " )) 
+sum2= int (input("Enter your value 2 " ))
+
+print("sum of both values are ",sum1 +sum2)
