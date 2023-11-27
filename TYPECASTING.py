e95061daf0b4bfbfe487f7f9cc6c32cc82e76499
Learning-stud <jayahalpara123456@gmail.com>
2023-11-27 16:13:53 +0530

    ......

---
+++ TYPECASTING.py
@@ -0,0 +1,19 @@
+a = 5
+b= 5
+c = 45
+
+a_str=str(a)
+b_str=str(b)
+c_str=str(c)
+
+
+final_strings =a_str + b_str + c_str
+
+finat_int = int(final_strings)
+
+print ("finat_int = ",finat_int)
+
+# to know its type 
+
+print ("finat_int = ",finat_int,type(finat_int))
+
---
+++ spherevolume.py
@@ -0,0 +1,12 @@
+ # taking radius as inout 
+ 
+radius =  float (input(" enter the radius of sphere: "))
+
+# calculation volume
+
+
+volume =( 4 / 3) * 3.1415 *( radius ** 3)
+
+# print vollume of sphere 
+
+print ("volume of sphere is : ", volume) 
