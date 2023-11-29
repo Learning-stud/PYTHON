951c0972861fe967b770eae56d1a8d0fb7f33fb2
Learning-stud <jayahalpara123456@gmail.com>
2023-11-30 05:14:49 +0530

    mintlify doc is usefull

--- define function.py
+++ define function.py
@@ -20,7 +20,19 @@
 ''' passing and function using parameter  '''
 
 def jay( majama):
+    """
+    The function "jay" prints a message with the input parameter "majama".
+    
+    :param majama: The parameter "majama" is a variable that represents some input or value that will be
+    passed into the function
+    """
     print(" ha samji gayo havwe vandho nai !",majama)
+
+# The line `devanshu = input("def function ma pehla hamesha print statement thase ane pachi def
+# function use thase ")` is taking user input and storing it in the variable `devanshu`. The user is
+# prompted with the message "def function ma pehla hamesha print statement thase ane pachi def
+# function use thase " and whatever they enter will be stored in `devanshu`.
+
 
 devanshu = input("def function ma pehla hamesha print statement thase ane pachi def function use thase ")
 
