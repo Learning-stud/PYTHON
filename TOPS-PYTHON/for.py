295dd1273c2be7e22661e95d9a3094021edc017a
Learning-stud <jayahalpara123456@gmail.com>
2023-11-30 01:17:16 +0530

    ....

---
+++ TOPS-PYTHON/for.py
@@ -0,0 +1,70 @@
+# for i in range(1,11):
+#     print(i,")","hello",sep="")
+    
+    
+# task 2  
+# sum =0 
+# for i in range(1,11):
+#     a= int( input("Enter no"))
+#     sum= sum+1
+     
+#     print(sum)
+    
+# task 3
+
+# oddsum =0
+# evensum= 0     
+# for i in range(1,11):
+#     a = int(input("enter odd no"))
+#     if (a%2 == 0):
+#         evensum =evensum+1
+#     else: 
+#         oddsum =oddsum+1
+    
+# print(oddsum,evensum)
+
+   
+   
+   #task 4  table of two
+   # by user input
+   # enter num :2
+   
+# for i in range(10):
+#     for j in range(10):
+#         if(i==4 and j==4):
+#            print("$", end=" ") 
+#         else:
+#          print("*", end=" ")
+         
+#     print()
+            
+            
+# for i in range(10):
+#     for j in range(10):
+#         if(i==j):
+#            print("$", end=" ") 
+#         else:
+#          print("*", end=" ")
+         
+#     print()
+
+# for i in range(10):
+#     for j in range(10):
+#         if( i>= j):
+#            print("$", end=" ") 
+#         else:
+#          print("*", end=" ")
+         
+#     print()
+    
+    
+# for i in range(5,1,-1):
+#     print ("* "*i)
+
+for i in range (1,11):
+    
+    if (i%2 == 0):
+        print("1")
+        
+    else:
+        print()
