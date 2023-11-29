2b889f3d24dc26ed422e3d8ca9e6e77981119126
Learning-stud <jayahalpara123456@gmail.com>
2023-11-30 01:31:40 +0530

    ..

--- celsiustoferhanite.py
+++ celsiustoferhanite.py
@@ -1,5 +1,12 @@
-celsius_temp=float( input (' enter temprature in celsius: '))
+# celsius_temp=float( input (' enter temprature in celsius: '))
 
-fah_temp = (celsius_temp* 9 / 5) + 32
+# fah_temp = (celsius_temp* 9 / 5) + 32
+
+# print ('temprature in farheinite: ', fah_temp)
+
+
+celsiu_temp = float( input( 'enter the temprature : '))
+
+fah_temp= (celsiu_temp * 9 / 5) + 32
 
 print ('temprature in farheinite: ', fah_temp)
