b70062ae7a8791ed3d0612b88176951b260aca4b
Learning-stud <jayahalpara123456@gmail.com>
2023-11-30 12:37:32 +0530

    ..

---
+++ grade.py
@@ -0,0 +1,12 @@
+grade = int(input(" enter the grade "))
+
+if grade >= 90 and grade <= 100:
+    print("SSaras nbov ajj saras  ")
+elif grade >= 80 and grade <= 90:
+    print("not bad not bot  ")
+elif grade >=70 and grade <= 80:
+    print("just there thai aaj gayu tu")
+elif  grade >=60 and grade <= 70:
+    print("vadhare try kar pan kosis acchi ki aapne")
+else:
+    print("Thai sansar 6e Next time ")         
