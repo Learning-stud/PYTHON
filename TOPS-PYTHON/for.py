a9c24c56e64c0f9fe50cd6bf6aa22f3e8f749be3
Learning-stud <jayahalpara123456@gmail.com>
2023-12-04 10:12:05 +0530

    .,.,.

--- TOPS-PYTHON/for.py
+++ TOPS-PYTHON/for.py
@@ -7,7 +7,6 @@
 # for i in range(1,11):
 #     a= int( input("Enter no"))
 #     sum= sum+1
-     
 #     print(sum)
     
 # task 3
@@ -24,18 +23,16 @@
 # print(oddsum,evensum)
 
    
-   
-   #task 4  table of two
-   # by user input
-   # enter num :2
-   
+#task 4  table of two
+# by user input
+# enter num :2
+
 # for i in range(10):
 #     for j in range(10):
 #         if(i==4 and j==4):
 #            print("$", end=" ") 
 #         else:
 #          print("*", end=" ")
-         
 #     print()
             
             
---
+++ TOPS-PYTHON/sortlistloop.py
@@ -0,0 +1,1 @@
+# sorting list loop User input 
