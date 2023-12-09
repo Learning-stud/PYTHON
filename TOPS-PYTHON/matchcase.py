38336dfc7b5107016d090fda533e74bd6ee6bf47
Learning-stud <jayahalpara123456@gmail.com>
2023-12-09 13:33:14 +0530

    match function in PYTHON

---
+++ TOPS-PYTHON/matchcase.py
@@ -0,0 +1,17 @@
+name = input("What's Your Name ? => : ")
+
+match name:
+ case "Devanshu":
+  print("Gomtipur")
+
+ case "Tirth":
+   print("rabari colony")
+
+ case "Dishha":
+  print("Gota")
+
+ case "Jay ":
+  print("Express Heighway")
+
+ case _:
+  print("Kai Baju thi")
