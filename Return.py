f840b43f4e54b5ef757dcef93096d3db4b7f57ab
Learning-stud <jayahalpara123456@gmail.com>
2023-12-09 13:19:41 +0530

    .,.,

--- Return.py
+++ Return.py
@@ -8,18 +8,19 @@
 
 # main()  # main() function ko call karo
 
+# The main function takes a number as input, calls the squared function to calculate the square of the number, and then prints the result.
 
 
 def main():
+
     number = int(input("Enter any number: "))
     result = squared(number)
     print("The Square of", number , "is", result )
 
 def squared(multi):
 
-    ''' 
-        Also can be written in this type its and python in build
-        function {return pow(multi, 2)}  
+    '''
+Also can be written in this type its and    python in build function {return pow(multi, 2)}
     '''
     return  multi * multi
 
