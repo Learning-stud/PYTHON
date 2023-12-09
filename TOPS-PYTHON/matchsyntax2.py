eea635ce84a578533c0b20e5ae5efb7ccfdc5f39
Learning-stud <jayahalpara123456@gmail.com>
2023-12-09 13:39:44 +0530

    with out case and elif in pyhon

---
+++ TOPS-PYTHON/matchsyntax2.py
@@ -0,0 +1,9 @@
+city = input("INput Your City Name : => : ")
+
+match city:
+  case " tirth" | "devanshu" | "jay":
+   print("Ahemdabad")
+  case "abhishek":
+   print("Dubai")
+  case _:
+   print("kya thi avyo")
