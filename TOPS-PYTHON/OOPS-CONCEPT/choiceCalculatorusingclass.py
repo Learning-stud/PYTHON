482847a4309364786c9a2d02bd2ec2671961a2c2
Learning-stud <jayahalpara123456@gmail.com>
2023-12-15 13:05:49 +0530

    user input calculator

--- TOPS-PYTHON/OOPS-CONCEPT/choiceCalculatorusingclass.py
+++ TOPS-PYTHON/OOPS-CONCEPT/choiceCalculatorusingclass.py
@@ -19,36 +19,48 @@ def divide(num1, num2):
 
 result = True
 
-print(
-    """---------User Choice--------
-        1.Addition
-        2.Substraction
-        3.Multiply
-        4.Divide
-       """
-)
 
-# user input
-num1 = int(input("Enter First Number : "))
-num2 = int(input("Enter Second Number : "))
 
-# User choice For Calciulation Of User Choice
+while(result):
+    print(
+        """---------User Choice--------
+            1.Addition
+            2.Substraction
+            3.Multiply
+            4.Divide
+        """
+    )
 
-choice = input("Enter Choice (1/2/3/4) : ")
+    # user input
+    num1 = int(input("Enter First Number : "))
+    num2 = int(input("Enter Second Number : "))
 
-if choice == "1":
-    result = add(num1, num2)
+    # User choice For Calciulation Of User Choice
 
-elif choice == "2":
-    result = sub(num1, num2)
+    choice = input("Enter Choice (1/2/3/4) : ")
 
-elif choice == "3":
-    result = multiply(num1, num2)
+    if choice == "1":
+        result = add(num1, num2)
 
-elif choice == "4":
-    result = divide(num1, num2)
+    elif choice == "2":
+        result = sub(num1, num2)
 
-else:
-    result = "Invalid Choice !"
+    elif choice == "3":
+        result = multiply(num1, num2)
 
-print(f"Results => {result}")
+    elif choice == "4":
+        result = divide(num1, num2)
+
+    else:
+        result = "Invalid Choice !"
+
+    print(f"Results => {result}")
+
+    user_choice=input("Do You Want To Continue (Y/N) : ")
+
+    if user_choice == "Y" or user_choice == "y":
+        result=True
+    else:
+        result=False
+
+
