8db3465d9dffec9e049d7677ae74798f9da0ad3d
Learning-stud <jayahalpara123456@gmail.com>
2023-12-20 15:55:08 +0530

    __init__ method in oops

---
+++ TOPS-PYTHON/pro.py
@@ -0,0 +1,14 @@
+class students:
+ id = 10
+ def __init__(self) :
+  print(" kem cho majam")
+
+ def input(self):
+  num = int (input( "Enter A number"))
+  self.id = num
+
+  def prompt(self):
+   print( " welcome aav aav ")
+
+  students()
+  students()
